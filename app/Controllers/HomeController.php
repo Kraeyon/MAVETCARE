026c7d1910741cf1022b47c401b76a99b666d73e
Kristofer <@bayononkristofergerard12@gmail.com>
2025-04-28 22:26:22 +0800

    home

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,6 +6,7 @@ class HomeController extends BaseController {
     public function homepage() {
         $this->render('home/homepage');
     }
+    // about page
     public function aboutpage() {
         $this->render('home/aboutpage');
     }
