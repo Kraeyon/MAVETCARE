fa8cc8916243d76e8263963db6eecc1719e7eab9
Kristofer <@bayononkristofergerard12@gmail.com>
2025-04-29 10:51:17 +0800

    added sticky header and added more in register

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -3,8 +3,11 @@
 <div class="container">
   <h2>Create an account</h2>
   <form method="POST" action="#">
-    <input type="email" name="email" placeholder="Email address">
-    <input type="password" name="password" placeholder="Password">
+    <input type="text" name="first_name" placeholder="First Name" required>
+    <input type="text" name="last_name" placeholder="Last Name" required>
+    <input type="email" name="email" placeholder="Email address" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <input type="password" name="confirm_password" placeholder="Confirm Password" required>
     <button type="submit">Register</button>
   </form>
   <p>Already have an account? <a href="/login">Login here</a></p>
